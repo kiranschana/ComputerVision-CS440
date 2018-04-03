e98af06cae091a25e8c6f78e851bc61b6d3fdf57
Kiran Chana <kiranschana@gmail.com>
2018-04-02 22:35:57 -0400

    integrated aaron's code and finished up game loop

--- tryingOpenCV.py
+++ tryingOpenCV.py
@@ -10,6 +10,7 @@ debug = True
 # OpenCV Variables
 cap = cv2.VideoCapture(0)
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
+sensitivity = 75
 
 # Game Variables
 states = ['idle', 'start', 'shoot', 'results']
@@ -24,7 +25,10 @@ pResult = "Lose"
 # Other Globals
 points = deque(maxlen=10) # contains recent positions of tracked objects
 dY = 0
-direction = '-'
+#direction = '-'
+directionList = deque(maxlen=15)
+directionList.appendleft('None')
+frameCounter = 15
 
 # Helper function for text
 def doubleText(frame, xy, text):
@@ -32,6 +36,49 @@ def doubleText(frame, xy, text):
 	cv2.putText(frame, text, (xy[0] - 2, xy[1] + 2), font, 1, (0,0,0), 2, cv2.LINE_AA)
 	cv2.putText(frame, text, (xy[0], xy[1]), font, 1, (255,255,255), 2, cv2.LINE_AA)
 
+# Functions for image recognition
+def feature_detect(img, template):
+	#img = cv2.imread(image,0)
+	temp = cv2.imread(template,0)
+	orb = cv2.ORB_create()
+	
+	ikp, des1 = orb.detectAndCompute(img,None)
+	tkp, des2 = orb.detectAndCompute(temp,None)
+	
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+	matches = bf.match(des1,des2)
+	avg_distance = 0
+	for i in range(0,5):
+		avg_distance += matches[i].distance
+	avg_distance /= 5
+	return avg_distance
+	
+def play_rps(img, mode):
+	rock = feature_detect(img, 'images/rock_template.jpg')
+	paper = feature_detect(img, 'images/paper_template.jpg')
+	scissors = feature_detect(img, 'images/scissors_template.jpg')
+	rps = [rock,paper,scissors]
+	if mode == 'win':
+		if min(rps) == rock:
+			return ('Rock!', 'Paper!')
+		elif min(rps) == paper:
+			return ('Paper!','Scissors!')
+		else:
+			return ('Scissors!','Rock!')
+	elif mode == 'draw':
+		if min(rps) == rock:
+			return ('Rock!', 'Rock!')
+		elif min(rps) == paper:
+			return ('Paper!', 'Paper!')
+		else:
+			return ('Scissors!', 'Scissors!')
+	else:
+		if min(rps) == rock:
+			return('Rock!', 'Scissors!')
+		elif min(rps) == paper:
+			return('Paper!', 'Rock!')
+		else:
+			return('Scissors!','Paper!')
 
 # Loop on frames of video
 while True:
@@ -53,26 +100,7 @@ while True:
 	#median2 = cv2.medianBlur(opened, 15)
 	frame2 = no_bg
 
-	# movement tracking 
-	cont_list = cv2.findContours(no_bg.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	if len(cont_list) > 0:
-		c = max(cont_list, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		#cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-		cv2.circle(frame, (int(x),int(y)), 5, (0, 0, 255), -1)
-		points.appendleft((x,y))
 
-	if len(points) == 10:
-		if points[0] is not None and points[-1] is not None:
-			dY = points[0][1] - points[-1][1]
-			if np.abs(dY) > 75:
-				if np.sign(dY) == -1:
-					direction = "UP"
-				else:
-					direction = "DOWN"
-			else:
-				direction = "NONE"
 
 	# Game Logic
 	if currentState == 'idle':
@@ -81,20 +109,64 @@ while True:
 		p1Choice = "Rock"
 		cpChoice = "Paper"
 		pResult = "Lose"
+		frameCounter = 15
 
-	# if currentState == 'start':
-		
+		## movement tracking 
 
-	# if currentState == 'shoot':
-		
+		# finding position of largest detected object, storing their coordinates
+		cont_list = cv2.findContours(frame2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		if len(cont_list) > 0:
+			c = max(cont_list, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			#cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+			cv2.circle(frame, (int(x),int(y)), 5, (0, 0, 255), -1)
+			points.appendleft((x,y))
 
-	# if currentState == 'results':
-		
+		# calculating the "speed" and assigning directions
+		if len(points) == 10:
+			if points[0] is not None and points[-1] is not None:
+				dY = points[0][1] - points[-1][1]
+				if np.abs(dY) > sensitivity:
+					if np.sign(dY) == -1:
+						if directionList[0] != "UP":
+							directionList.appendleft("UP")
+					elif directionList[0] != "DOWN":
+						directionList.appendleft("DOWN")
+				elif directionList[0] != "NONE":
+					directionList.appendleft("NONE")
+					# Motion has just stopped. Lets check if the player has started the game
+					dlist = [x for x in directionList if x!="NONE"][:7]
+					if dlist == ["DOWN", "UP", "DOWN", "UP", "DOWN", "UP", "DOWN"]:
+						currentState = states[1]
+
+	if currentState == 'start':
+		# Call function
+		roi = frame[320:960, 180:540]
+		(p1Choice, cpChoice) = play_rps(roi, 'win')
+		currentState = states[2]
+
+	if currentState == 'shoot':
+		if frameCounter == 0:
+			currentState = states[3]
+			frameCounter = 60
+			cpScore += 1
+		else:
+			frameCounter -= 1
+
+	if currentState == 'results':
+		if frameCounter == 0:
+			currentState = states[0]
+		else:
+			frameCounter -= 1
+		directionList = deque(maxlen=15)
+		directionList.appendleft('None')
 
 	# UI For Demo
 	if debug:
 		doubleText(frame, (30, 650), "Game state: " + currentState)
-		doubleText(frame, (30, 200), direction)
+		doubleText(frame, (30, 200), directionList[0])
+		doubleText(frame, (30, 240), str(directionList))
+
 
 	doubleText(frame, (30,30), "Play Rock Paper Scissors!")
 	doubleText(frame, (30,70), "Player Score: " + str(p1Score))
